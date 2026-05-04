f0035ea8a1b956b4e472d269be96e962e94beb16
sumonkumardey <oksumondey153@gmail.com>
2026-05-04 08:25:26 +0600

    git push https://sumonkumardey23-wq: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@github.com/sumonkumardey23-wq/glixadd.git mainRevert "fix sw.js zone id"
    This reverts commit 69030cb1145b9a12f469688e6a6d8e3843b86522.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10958198
+    "zoneId": 10957831
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
