69030cb1145b9a12f469688e6a6d8e3843b86522
sumonkumardey <oksumondey153@gmail.com>
2026-05-04 08:00:45 +0600

    fix sw.js zone id

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10957831
+    "zoneId": 10958198
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
